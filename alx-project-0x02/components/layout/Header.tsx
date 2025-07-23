5c7ac043e41f859ed6f88a269a1513c1ff21defd
Wilfred Osozi <osoziw@gmail.com>
2025-07-24 00:33:26 +0300

    added header navigation links

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,14 +1,13 @@
-import Home from "@/pages";
-import About from "@/pages/about";
+import React from "react";
+import Link from "next/link";
 
 
 const Header: React.FC =()=>{
 
     return(
         <header>
-            <p>Project Header</p>
-            <Home />
-            <About />
+        <Link href= '/home' className="bg-blue-400 p-3">Home</Link>
+        <Link href= '/about' className="bg-blue-400 p-3" >About</Link>
         </header>
     )
 }
