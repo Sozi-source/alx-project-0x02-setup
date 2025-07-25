6f0c0fae04011ae6360fd8c377f6bbf2b6b33568
Wilfred Osozi <osoziw@gmail.com>
2025-07-25 22:48:47 +0300

    style the button

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -5,9 +5,11 @@ import Link from "next/link";
 const Header: React.FC =()=>{
 
     return(
-        <header>
-        <Link href= '/home' className="bg-blue-400 p-3">Home</Link>
-        <Link href= '/about' className="bg-blue-400 p-3" >About</Link>
+        <header className="fixed top-0 w-full z-50 bg-white text-blue-400">
+        <div className="flex items-center justify-center gap-5">
+            <Link href= '/home' className="p-3">Home</Link>
+            <Link href= '/about' className="p-3" >About</Link>
+        </div>
         </header>
     )
 }
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import Button from "@/components/common/Button";
 
 const About: React.FC =()=>{
     return(
         <div>
             <h1>About Page</h1>
 
-            <div>
-                <button>Small</button>
+            <div className="flex items-center gap-5 p-5">
+                <Button size ="small" shape = "rounded-sm"className="bg-blue-300 text-white border border-solid-gray-500">Small Button</Button>
 
-                <button>Medium</button>
+                <Button size ="medium" shape = "rounded-md"className="bg-blue-300 text-white border border-solid-gray-500">Medium Button</Button>
 
-                <button> Large</button>
+                <Button size ="large" shape = "rounded-full"className="bg-blue-300 text-white border border-solid-gray-500"> Large Button</Button>
             </div>
         </div>
     )
