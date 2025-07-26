78b0d128bc8011b64ef0e1cf84306ac1105d21f9
Wilfred Osozi <osoziw@gmail.com>
2025-07-26 23:23:24 +0300

    updated the user.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -26,7 +26,7 @@ return(
 )
 }
 
-export const getStaticProps: GetStaticProps = async()=>{
+export const getStaticProps: GetStaticProps<userPageProps> = async()=>{
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const users: UserProps[]= await response.json();
 
